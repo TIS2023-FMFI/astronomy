b2742761a0c4cadf8e9d858e48073a5752c4ec72
Benjamin Bergstrom <benko.bergstrom@outlook.com>
2023-11-29 16:00:35 +0100

    Reset

---
+++ Programme.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Astronomy
+{
+    internal class Programme {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wef");
+        }
+    }
+}
